a4f1a1cc3c032407a58714068e9891ca5ae29bd5
sha-17 <shahussain.arif@gmail.com>
2021-03-05 21:02:49 +0530

    bank

---
+++ Bank.java
@@ -0,0 +1,22 @@
+package com.ajin.org;
+
+import java.util.Scanner;
+
+public class Bank {
+	public static void main(String[] args) {
+		
+		Scanner eb = new Scanner(System.in);
+		   System.out.println("enter your address");
+		   String adress = eb.nextLine();
+		   System.out.println("your address:"+ adress);
+		   
+		   System.out.println("enter your id");
+		   int id = eb.nextInt();
+		   System.out.println("your id is:"+ id);
+	}
+
+
+   
+
+	
+}
